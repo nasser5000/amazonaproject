52f8c5a3f162d6980d0fd664e716578d0397af46
Nasser Salem <nassersalemf5000@gmail.com>
2021-12-11 15:43:58 +0200

    convert staic file to dynamic file with react

---
+++ frontend/src/data.js
@@ -0,0 +1,67 @@
+const data = {
+    products: [
+        {
+            _id: '1',
+            name: 'Nike Slime Shirt',
+            image: '/images/p1.jpg',
+            price: 120,
+            brand: 'Nike',
+            rating: 4.5,
+            numReviews: 10,
+            description: 'Heigh quality products',
+        },
+        {
+            _id: '2',
+            name: 'Nike Slime Shirt',
+            image: '/images/p2.jpg',
+            price: 80,
+            brand: 'Nike',
+            rating: 4.3,
+            numReviews: 20,
+            description: 'Heigh quality products',
+        },
+        {
+            _id: '3',
+            name: 'Nike Slime Shirt',
+            image: '/images/p3.jpg',
+            price: 90,
+            brand: 'Nike',
+            rating: 4.1,
+            numReviews: 8,
+            description: 'Heigh quality products',
+        },
+        {
+            _id: '4',
+            name: 'Nike Slime Shirt',
+            image: '/images/p1.jpg',
+            price: 88,
+            brand: 'Nike',
+            rating: 4.3,
+            numReviews: 7,
+            description: 'Heigh quality products',
+        },
+        {
+            _id: '5',
+            name: 'Nike Slime Shirt',
+            image: '/images/p2.jpg',
+            price: 80,
+            brand: 'Nike',
+            rating: 4.4,
+            numReviews: 9,
+            description: 'Heigh quality products',
+        },
+        {
+            _id: '6',
+            name: 'Nike Slime Shirt',
+            image: '/images/p3.jpg',
+            price: 55,
+            brand: 'Nike',
+            rating: 4.5,
+            numReviews: 6,
+            description: 'Heigh quality products',
+        },
+    ],
+}
+
+
+export default data ;
